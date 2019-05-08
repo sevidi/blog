1c417177e189275e2f4b78f703b7eafacd2fcf58
Victor Smirnov <istern24@mail.ru>
2019-05-08 19:22:26 +0300

    Added contact service

--- frontend/tests/unit/forms/ContactFormTest.php
+++ frontend/tests/unit/forms/ContactFormTest.php
@@ -2,14 +2,13 @@
 namespace frontend\tests\unit\forms;
 
 use frontend\forms\ContactForm;
-use yii\mail\MessageInterface;
+
 
 class ContactFormTest extends \Codeception\Test\Unit
 {
-    public function testSendEmail()
+    public function testSuccess()
     {
         $model = new ContactForm();
-
         $model->attributes = [
             'name' => 'Tester',
             'email' => 'tester@example.com',
@@ -17,18 +16,6 @@ class ContactFormTest extends \Codeception\Test\Unit
             'body' => 'body of current message',
         ];
 
-        expect_that($model->sendEmail('admin@example.com'));
-
-        // using Yii2 module actions to check email was sent
-        $this->tester->seeEmailIsSent();
-
-        /** @var MessageInterface  $emailMessage */
-        $emailMessage = $this->tester->grabLastSentEmail();
-        expect('valid email is sent', $emailMessage)->isInstanceOf('yii\mail\MessageInterface');
-        expect($emailMessage->getTo())->hasKey('admin@example.com');
-        expect($emailMessage->getFrom())->hasKey('noreply@example.com');
-        expect($emailMessage->getReplyTo())->hasKey('tester@example.com');
-        expect($emailMessage->getSubject())->equals('very important letter subject');
-        expect($emailMessage->toString())->contains('body of current message');
+        expect_that($model->validate());
     }
 }
