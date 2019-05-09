0ca7a66fb808c874d9ee2ba73431548049bd2e64
Victor Smirnov <istern24@mail.ru>
2019-05-09 12:29:11 +0300

    Added auth service

--- frontend/tests/functional/LoginCest.php
+++ frontend/tests/functional/LoginCest.php
@@ -1,31 +1,19 @@
 <?php
 
 namespace frontend\tests\functional;
-
 use frontend\tests\FunctionalTester;
 use common\fixtures\UserFixture;
 
 class LoginCest
 {
-    /**
-     * Load fixtures before db transaction begin
-     * Called in _before()
-     * @see \Codeception\Module\Yii2::_before()
-     * @see \Codeception\Module\Yii2::loadFixtures()
-     * @return array
-     */
-    public function _fixtures()
-    {
-        return [
-            'user' => [
-                'class' => UserFixture::className(),
-                'dataFile' => codecept_data_dir() . 'login_data.php',
-            ],
-        ];
-    }
-
     public function _before(FunctionalTester $I)
     {
+        $I->haveFixtures([
+            'user' => [
+                'class' => UserFixture::className(),
+                'dataFile' => codecept_data_dir() . 'login_data.php'
+            ]
+        ]);
         $I->amOnRoute('site/login');
     }
 
@@ -47,14 +35,9 @@ class LoginCest
     public function checkWrongPassword(FunctionalTester $I)
     {
         $I->submitForm('#login-form', $this->formParams('admin', 'wrong'));
-        $I->seeValidationError('Incorrect username or password.');
+        $I->see('Incorrect username or password.');
     }
 
-    public function checkInactiveAccount(FunctionalTester $I)
-    {
-        $I->submitForm('#login-form', $this->formParams('test.test', 'Test1234'));
-        $I->seeValidationError('Incorrect username or password');
-    }
 
     public function checkValidLogin(FunctionalTester $I)
     {
