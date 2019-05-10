cef73cba598e98c1d2c5211520d90c34a7ba1531
Victor Smirnov <istern24@mail.ru>
2019-05-10 20:18:33 +0300

    Added blog categories and CompositeForm

---
+++ post/repositories/Blog/CategoryRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace post\repositories\Blog;
+
+use post\entities\Blog\Category;
+use post\repositories\NotFoundException;
+
+class CategoryRepository
+{
+    public function get($id): Category
+    {
+        if (!$category = Category::findOne($id)) {
+            throw new NotFoundException('Category is not found.');
+        }
+        return $category;
+    }
+
+    public function save(Category $category): void
+    {
+        if (!$category->save()) {
+            throw new \RuntimeException('Saving error.');
+        }
+    }
+
+    public function remove(Category $category): void
+    {
+        if (!$category->delete()) {
+            throw new \RuntimeException('Removing error.');
+        }
+    }
+
+}
---
+++ post/services/manage/Blog/CategoryManageService.php
@@ -0,0 +1,62 @@
+<?php
+
+
+namespace post\services\manage\Blog;
+
+use post\entities\Meta;
+use post\entities\Blog\Category;
+use post\forms\manage\Blog\CategoryForm;
+use post\repositories\Blog\CategoryRepository;
+
+class CategoryManageService
+{
+    private $categories;
+
+    public function __construct(CategoryRepository $categories)
+    {
+        $this->categories = $categories;
+    }
+
+    public function create(CategoryForm $form): Category
+    {
+        $category = Category::create(
+            $form->name,
+            $form->slug,
+            $form->title,
+            $form->description,
+            $form->sort,
+            new Meta(
+                $form->meta->title,
+                $form->meta->description,
+                $form->meta->keywords
+            )
+        );
+        $this->categories->save($category);
+        return $category;
+    }
+
+    public function edit($id, CategoryForm $form): void
+    {
+        $category = $this->categories->get($id);
+        $category->edit(
+            $form->name,
+            $form->slug,
+            $form->title,
+            $form->description,
+            $form->sort,
+            new Meta(
+                $form->meta->title,
+                $form->meta->description,
+                $form->meta->keywords
+            )
+        );
+        $this->categories->save($category);
+    }
+
+    public function remove($id): void
+    {
+        $category = $this->categories->get($id);
+        $this->categories->remove($category);
+    }
+
+}
