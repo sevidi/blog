135eafd79183a4f266b2e97dceb234aa10654def
Victor Smirnov <istern24@mail.ru>
2019-05-11 18:49:23 +0300

    Added pages

---
+++ post/readModels/PageReadRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace post\readModels;
+
+use post\entities\Page;
+
+class PageReadRepository
+{
+
+    public function find($id): ?Page
+    {
+        return Page::findOne($id);
+    }
+
+    public function findBySlug($slug): ?Page
+    {
+        return Page::find()->andWhere(['slug' => $slug])->andWhere(['>', 'depth', 0])->one();
+    }
+
+}
