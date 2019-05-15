3e619980cb7e34d3ea1a7c5126578f8cee06749a
Victor Smirnov <istern24@mail.ru>
2019-05-15 17:51:03 +0300

    Added ELFinder

---
+++ backend/controllers/FileController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace backend\controllers;
+
+use yii\web\Controller;
+
+class FileController extends Controller
+{
+    /**
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+}
